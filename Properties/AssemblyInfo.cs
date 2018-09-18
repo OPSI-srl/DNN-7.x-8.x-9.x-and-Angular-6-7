f6fb4663b6bf813d7a162d94c23eecdc0453edad
Matteo Benedusi <matteo.benedusi@opsi.it>
2018-09-18 12:22:51 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Angularmodule")]
+[assembly: AssemblyDescription("Sample module for the ANgular module blog series on blog.bitboxx.net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("bitboxx solutions")]
+[assembly: AssemblyProduct("Angularmodule")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("all rights reserved")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("258642f3-e1f4-45af-b77a-c57cec73c0ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ View.ascx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Resources;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Security;
+using DotNetNuke.Security.Permissions;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.UI.Utilities;
+using Newtonsoft.Json;
+
+namespace Angularmodule
+{
+    [DNNtc.ModuleDependencies(DNNtc.ModuleDependency.CoreVersion, "07.00.00")]
+    [DNNtc.PackageProperties("Angularmodule", 1, "Angular module", "An Angular6 template", "BBAngular.png", "Torsten Weggen", "OPSI solutions", "http://www.opsi.it", "support@bopsi.it", false)]
+    [DNNtc.ModuleProperties("Angularmodule", "Angular module", 0)]
+    [DNNtc.ModuleControlProperties("", "Angularmodule", DNNtc.ControlType.View, "", false, false)]
+    public partial class View : PortalModuleBase
+    {
+        protected string Users
+        {
+            get
+            {
+                var users = UserController.GetUsers(PortalId).Cast<UserInfo>()
+                    .Select(u => new { text = u.Username, id = u.UserID });
+                return ClientAPI.GetSafeJSString(JsonConvert.SerializeObject(users));
+            }
+        }
+
+        protected string Resources
+        {
+            get
+            {
+                using (var rsxr = new ResXResourceReader(MapPath(LocalResourceFile + ".ascx.resx")))
+                {
+                    var res = rsxr.OfType<DictionaryEntry>()
+                        .ToDictionary(
+                            entry => entry.Key.ToString().Replace(".", "_"),
+                            entry => LocalizeString(entry.Key.ToString()));
+
+                    return ClientAPI.GetSafeJSString(JsonConvert.SerializeObject(res));
+                }
+
+            }
+        }
+
+        protected string ModuleSettings
+        {
+            get
+            {
+                return ClientAPI.GetSafeJSString(JsonConvert.SerializeObject(Settings));
+            }
+        }
+
+        protected bool Editable
+        {
+            get { return IsEditable && EditMode; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DotNetNuke.Framework.ServicesFramework.Instance.RequestAjaxScriptSupport();
+                DotNetNuke.Framework.ServicesFramework.Instance.RequestAjaxAntiForgerySupport();
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        protected string version
+        {
+            get
+            {
+                //return "?v=" + this.PortalSettings.CdfVersion;
+                return "?v=" + DateTime.Now.Ticks;
+            }
+        }
+    }
+}
